39a1c72030af500707de08d9455722f2325bca7e
Masaki Murooka <m-murooka@aist.go.jp>
2022-06-13 12:05:41 +0900

    add Dataset.cpp

---
+++ include/DDMPC/Dataset.h
@@ -0,0 +1,103 @@
+/* Author: Masaki Murooka */
+
+#pragma once
+
+#include <torch/torch.h>
+
+namespace DDMPC
+{
+/*! \brief Class of single data for state equation. */
+class Data
+{
+public:
+  /*! \brief Constructor.
+   *  \param state single data tensor of current state
+   *  \param input single data tensor of current input
+   *  \param next_state single data tensor of next state
+   */
+  Data(const torch::Tensor & state, const torch::Tensor & input, const torch::Tensor & next_state)
+  : state_(state), input_(input) next_state_(next_state),
+  {
+  }
+
+public:
+  /*! \brief Single data tensor of current state. */
+  torch::Tensor state_;
+
+  /*! \brief Single data tensor of current input. */
+  torch::Tensor input_;
+
+  /*! \brief Single data tensor of next state. */
+  torch::Tensor next_state_;
+};
+
+/*! \brief Single data example of state equation. */
+using Example = torch::data::Example<Data, torch::data::example::NoTarget>;
+
+/*! \brief Class of dataset for state equation. */
+class Dataset : public torch::data::datasets::Dataset<Dataset, Example>
+{
+public:
+  /*! \brief Constructor.
+   *  \param state all data tensor of current state
+   *  \param input all data tensor of current input
+   *  \param next_state all data tensor of next state
+   */
+  explicit Dataset(const torch::Tensor & state, const torch::Tensor & input, const torch::Tensor & next_state)
+  : state_(std::move(state)), input_(std::move(input)), next_state_(std::move(next_state))
+  {
+    // Store size because tensors are passed to data loader with std::move
+    size_ = state_.size(0);
+  }
+
+  /*! \brief Returns a single data example. */
+  inline Example get(size_t index) override
+  {
+    return Data(state_[index], input_[index], next_state_[index]);
+  }
+
+  /*! \brief Returns dataset size. */
+  inline torch::optional<size_t> size() const override
+  {
+    return size_;
+  }
+
+protected:
+  /*! \brief All data tensor of current state. */
+  torch::Tensor state_;
+
+  /*! \brief All data tensor of current input. */
+  torch::Tensor input_;
+
+  /*! \brief All data tensor of next state. */
+  torch::Tensor next_state_;
+
+  /*! \brief Dataset size. */
+  size_t size_;
+};
+
+/*! \brief Make dataset.
+ *  \param state all data tensor of current state
+ *  \param input all data tensor of current input
+ *  \param next_state all data tensor of next state
+ *  \param[out] train_dataset training dataset
+ *  \param[out] test_dataset test dataset
+ */
+void makeDataset(const torch::Tensor & state,
+                 const torch::Tensor & input,
+                 const torch::Tensor & next_state std::shared_ptr<StateEqDataset> & train_dataset,
+                 std::shared_ptr<StateEqDataset> & test_dataset);
+
+/*! \brief Make batch tensors.
+ *  \param batch batch from data loader
+ *  \param device device to which tensors belong
+ *  \param[out] b_state batch tensor of current state
+ *  \param[out] b_next_state batch tensor of next state
+ *  \param[out] b_input batch tensor of input
+ */
+void makeBatchTensor(const std::vector<StateEqExample> & batch,
+                     const torch::Device & device,
+                     torch::Tensor & b_state,
+                     torch::Tensor & b_input,
+                     torch::Tensor & b_next_state);
+} // namespace DDMPC
---
+++ src/Dataset.cpp
@@ -0,0 +1,63 @@
+/* Author: Masaki Murooka */
+
+#include <Dataset.h>
+
+using namespace DDMPC;
+
+void DDMPC::makeDataset(const torch::Tensor & state,
+                        const torch::Tensor & input,
+                        const torch::Tensor & next_state std::shared_ptr<StateEqDataset> & train_dataset,
+                        std::shared_ptr<StateEqDataset> & test_dataset)
+{
+  // Make dataset for train and test
+  int n_all = state.size(0);
+  int n_train = 0.7 * n_all;
+  int n_test = n_all - n_train;
+  torch::Tensor perm = torch::randperm(n_all, torch::kInt64);
+  torch::Tensor train_perm = perm.index({at::indexing::Slice(0, n_train)});
+  torch::Tensor test_perm = perm.index({at::indexing::Slice(n_train, n_train + n_test)});
+  train_dataset = std::make_shared<StateEqDataset>(state.index({train_perm}), input.index({train_perm}),
+                                                   next_state.index({train_perm}));
+  test_dataset = std::make_shared<StateEqDataset>(state.index({test_perm}), input.index({test_perm}),
+                                                  next_state.index({test_perm}));
+
+  // Print debug information
+  constexpr bool debug = true;
+  if(debug)
+  {
+    std::cout << "Construct Dataset" << std::endl;
+    std::cout << "  - train_dataset size: " << train_dataset->size().value() << std::endl
+              << "  - test_dataset size: " << test_dataset->size().value() << std::endl;
+  }
+}
+
+void DDMPC::makeBatchTensor(const std::vector<StateEqExample> & batch,
+                            const torch::Device & device,
+                            torch::Tensor & b_state,
+                            torch::Tensor & b_input,
+                            torch::Tensor & b_next_state)
+{
+  int batch_size = batch.size();
+
+  // Allocate batch tensors
+  {
+    const auto & data = static_cast<StateEqData>(batch[0]);
+    b_state = torch::empty({batch_size, data.state_.size(1)});
+    b_input = torch::empty({batch_size, data.input_.size(1)});
+    b_next_state = torch::empty({batch_size, data.next_state_.size(1)});
+  }
+
+  // Set batch tensors
+  for(int i = 0; i < batch_size; i++)
+  {
+    const auto & data = static_cast<StateEqData>(batch[i]);
+    b_state.index({i}) = data.state_;
+    b_input.index({i}) = data.input_;
+    b_next_state.index({i}) = data.next_state_;
+  }
+
+  // Send to device
+  b_state = b_state.to(device);
+  b_input = b_input.to(device);
+  b_next_state = b_next_state.to(device);
+}
