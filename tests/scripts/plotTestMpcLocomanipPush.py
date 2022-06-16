8823cb011b3c4815f47828cd0ddac016a8100fca
Masaki Murooka <m-murooka@aist.go.jp>
2022-06-16 15:51:09 +0900

    plot damper forces.

--- tests/scripts/plotTestMpcLocomanipPush.py
+++ tests/scripts/plotTestMpcLocomanipPush.py
@@ -12,7 +12,7 @@ class PlotTestMpcLocomanipPush(object):
         fig = plt.figure()
         plt.rcParams["font.size"] = 16
 
-        ax = fig.add_subplot(211)
+        ax = fig.add_subplot(311)
         ax.plot(self.result_data_list["time"], self.result_data_list["robot_com_pos"],
                 color="green", label="planned robot pos")
         ax.plot(self.result_data_list["time"], self.result_data_list["obj_com_pos"],
@@ -28,13 +28,25 @@ class PlotTestMpcLocomanipPush(object):
         ax.grid()
         ax.legend(loc="upper left")
 
-        ax = fig.add_subplot(212)
+        ax = fig.add_subplot(312)
         ax.plot(self.result_data_list["time"], self.result_data_list["obj_force"],
                 color="green", label="obj force")
         ax.set_xlabel("time [s]")
         ax.set_ylabel("force [N]")
         ax.grid()
-        ax.legend(loc="upper left")
+        ax.legend(loc="upper right")
+
+        mass = 50.0 # [kg]
+        damper_forces = mass * (self.result_data_list["obj_com_vel"][1:] - self.result_data_list["obj_com_vel"][:-1]) / \
+                        (self.result_data_list["time"][1:] - self.result_data_list["time"][:-1]) \
+                        - self.result_data_list["obj_force"][:-1]
+        ax = fig.add_subplot(313)
+        ax.scatter(self.result_data_list["obj_com_vel"][:-1], damper_forces,
+                   color="green", label="damper force")
+        ax.set_xlabel("vel [m/s]")
+        ax.set_ylabel("force [N]")
+        ax.grid()
+        ax.legend(loc="upper right")
 
         plt.show()
 
